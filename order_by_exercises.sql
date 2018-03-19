8ea7d517e02a91a455788598e1b535661eb6a1e8
davidalex620 <spand3r@gmail.com>
2018-03-19 16:51:34 -0500

    MySQL order_by_exercises.sql

---
+++ order_by_exercises.sql
@@ -0,0 +1,31 @@
+USE employees;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE ('E%')
+ORDER BY emp_no DESC;
+
+
+SELECT birth_date, hire_date, first_name, last_name
+FROM employees
+WHERE birth_date LIKE '____-12-25'
+      AND hire_date like '199%'
+ORDER BY birth_date, hire_date DESC;
